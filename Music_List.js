d70df9a100ec468f44959ca24230bbd88b29c050
ATHUL THOMAS P <147591846+athulthomas4@users.noreply.github.com>
2023-11-05 21:00:51 +0530

    Add files via upload

---
+++ Music_List.js
@@ -0,0 +1,50 @@
+let allMusic = [
+    {
+        name: "Amme amme thaaye",
+        artist: "kester",
+        img: "image-1",
+        src: "Amme amme thaaye"
+    },
+    {
+        name: "Amme mathaave nin kayyilirikkunna",
+        artist: "Fr.Binoj Mulavarical",
+        img: "image-2",
+        src: "Amme mathaave nin kayyilirikkunna"
+    },
+    {
+        name: "Athmasakthiye irangi ennil vaa",
+        artist: "Reji Narayanan",
+        img: "image-3",
+        src: "Athmasakthiye irangi ennil vaa"
+    },
+    {
+        name: "Dhaivakripayil Njan Ashrayichu",
+        artist: "Candles Band",
+        img: "image-4",
+        src: "Dhaivakripayil Njan Ashrayichu"
+    },
+    {
+        name: "Enikkai Karuthunnavan",
+        artist: "Candles Band",
+        img: "image-5",
+        src: "Enikkai Karuthunnavan"
+    },
+    {
+        name: "Enneshunadhante Nadhante Padhathinkal",
+        artist: "Candles Band",
+        img: "image-6",
+        src: "Enneshunadhante Nadhante Padhathinkal"
+    },
+    {
+        name: "Kashtangal Saramilla",
+        artist: "Candles Band",
+        img: "image-7",
+        src: "Kashtangal Saramilla"
+    },
+    {
+        name: "Sthuthi Sthuthi Enn maname",
+        artist: "Candles Band",
+        img: "image-8",
+        src: "Sthuthi Sthuthi Enn maname"
+    },
+]
